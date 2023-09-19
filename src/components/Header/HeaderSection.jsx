e9a6607c213e3e31072af29f2d09fdd4d2d485ac
Daniel <92042414+DAg2no@users.noreply.github.com>
2023-09-19 13:00:08 -0300

    tema añadido

--- src/components/Header/HeaderSection.jsx
+++ src/components/Header/HeaderSection.jsx
@@ -1,23 +1,24 @@
 import Grid from "@mui/material/Grid";
-import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
 import ThemeButton from "../../theme/themeButton";
+import Typography from '@mui/material/Typography'
 
 
-
-const HeaderSection = ({toggleTheme}) => {
+const HeaderSection = ({ toggleTheme }) => {
   return (
     <>
+    
       <Grid>
         <AppBar position="fixed" color="inherit">
           <Toolbar>
-            
-              <ThemeButton toggleTheme={toggleTheme}/>
-            
+            <ThemeButton toggleTheme={toggleTheme} />
+            <Typography variant="h2" color='inherit'></Typography>
           </Toolbar>
         </AppBar>
-          
       </Grid>
+      
+    
     </>
   );
 };
