0e68d894e8d87fc9ce1a76fc31dc7ccf731ca571
Daniel <92042414+DAg2no@users.noreply.github.com>
2023-11-18 16:45:58 -0300

    ...

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -16,8 +16,8 @@ import { Link } from "react-router-dom";
  export const NavBar = ({ toggleDrawer, toggleTheme }) => {
 
     const renderNavButtons = () => {
-      return navLinks.map((link, index) => (
-        <Link key={index} to={link.link} style={{textDecoration: 'none', color: 'inherit',}}>
+      return navLinks.map((link) => (
+        <Link key={link.link} to={link.link} style={{textDecoration: 'none', color: 'inherit',}}>
           <Typography variant="h6" color="inherit" fontSize="1.1rem">
             {link.text}
           </Typography>
