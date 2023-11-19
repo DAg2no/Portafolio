d3f843690ab9eaa9697e520f038be15ac91f9baf
Daniel <92042414+DAg2no@users.noreply.github.com>
2023-11-19 20:29:47 -0300

    ...

--- src/helpers/navLinks.js
+++ src/helpers/navLinks.js
@@ -3,9 +3,9 @@
 
 export const navLinks = [
     { text: "Home", link: "/" },
-    { text: "Project", link: "/#/project"},
-    { text: "About", link: '/#/about'},
-    { text: "Contact", link: "/#/Contact"},
+    { text: "Project", link: "/project"},
+    { text: "About", link: '/about'},
+    { text: "Contact", link: "/Contact"},
   ];
 
 
